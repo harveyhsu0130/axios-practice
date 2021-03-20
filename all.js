0e4d2ead5abdb98de0a78bfd4d855052349da1d9
harvey <a0958013032@gmail.com>
2021-03-20 19:07:05 +0800

    axios

---
+++ all.js
@@ -0,0 +1,109 @@
+
+
+
+axios.get(`https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json`)
+.then(function (response) {
+   
+
+travelData=response.data.data;
+showTravelInfo(travelData);//首次渲染
+});
+
+let travelData=[];
+  
+  let travelInfo='';
+  let travelNum=0;
+  const list = document.querySelector('ul');
+  const searchLocation=document.querySelector('.searchArea select');
+  const showSearchNum=document.querySelector('.searchArea p');
+  const addBtn=document.querySelector('.btn');
+  const travelName=document.querySelector('.travelName');
+  const travelImg=document.querySelector('.travelImg');
+  const travelLocation=document.querySelector('.travelLocation');
+  const travelPrice=document.querySelector('.travelPrice');
+  const travelLevel=document.querySelector('.travelLevel');
+  const travelGroup=document.querySelector('.travelGroup');
+  const travelText=document.querySelector('.travelText');
+
+  
+
+
+  //新增套票
+  addBtn.addEventListener('click',function(){
+    if(searchLocation.value==''||travelName.value==''||travelImg.value==''||travelLocation.value==''
+    ||travelPrice.value==''||travelLevel.value==''||travelGroup.value==''||travelText.value==''){
+        alert('請確實填寫所有欄位');
+        
+    }
+    else if(travelLevel.value>10||travelLevel.value<1){
+        alert('套票星級必須在1~10之間');
+    }else{
+      let obj={};
+      obj.id=data.length;
+      obj.name=travelName.value;
+      obj.imgUrl=travelImg.value;
+      obj.area=travelLocation.value;
+      obj.description=travelText.value;
+      obj.group=Number(travelGroup.value);
+      obj.price=Number(travelPrice.value);
+      obj.rate=Number(travelLevel.value);
+      data.push(obj);
+      showTravelInfo();
+      showSearchNum.textContent=``;
+      searchLocation.value="地區搜尋";
+      travelName.value='';
+      travelImg.value='';
+      travelLocation.value='選擇景點地區';
+      travelText.value='';
+      travelGroup.value='';
+      travelPrice.value='';
+      travelLevel.value='';
+    }
+  })
+
+
+  //顯示套票
+  function showTravelInfo(travelData){
+      travelInfo='';
+      travelData.forEach(function(item,index){
+    travelInfo+=
+    `
+    <li>
+    <span class="location">${item.area}</span>
+    <span class="starlevel">${item.rate}</span>
+    <img src="${item.imgUrl}"alt="">
+    <div class="travelDescription">
+        <h2>${item.name}</h2>
+        <p>${item.description}</p>
+        <div class="travelCost">
+            <span><i class="fas fa-exclamation-circle"></i>剩下最後 ${item.group} 組</span>
+            <p>TWD<span>${item.price}</span></p>
+        </div>
+    </div>
+</li>
+    `
+  })
+  list.innerHTML=travelInfo;
+}
+
+
+//搜尋套票
+searchLocation.addEventListener('change',function(e){
+
+  let areaFilter=[];
+  travelData.forEach(function(item){
+    if(e.target.value==item.area){
+      areaFilter.push(item);
+      showTravelInfo(areaFilter);
+      showSearchNum.textContent=`本次搜尋共 ${areaFilter.length} 筆資料`;
+    }else if(e.target.value=="全部地區"){
+      showTravelInfo(travelData);
+      showSearchNum.textContent=`本次搜尋共 ${data.length} 筆資料`;
+    }
+  })
+  
+})
+
+
+
+
